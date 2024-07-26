ce54b2ce9d86cfb77865c08c683b26c53fd33809
unknown <hiteshdullu@gmail.com>
2024-07-26 17:10:16 +0530

    Taking player details as input

--- src/main/java/org/magicalarena/GameMain.java
+++ src/main/java/org/magicalarena/GameMain.java
@@ -4,15 +4,35 @@ import org.magicalarena.model.Arena;
 import org.magicalarena.model.Player;
 import org.magicalarena.service.GameService;
 
+import java.util.Scanner;
+
 public class GameMain {
     public static void main(String[] args) {
-        Player playerA = new Player(50, 5, 10);
-        Player playerB = new Player(100, 10, 5);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter details for Player A:");
+        Player playerA = createPlayer(scanner);
+
+        System.out.println("Enter details for Player B:");
+        Player playerB = createPlayer(scanner);
 
         Arena arena = new Arena(playerA, playerB);
         GameService gameService = new GameService();
         Player winner = gameService.fight(arena);
 
         gameService.printWinner(winner, playerA, playerB);
+    }
+
+    private static Player createPlayer(Scanner scanner) {
+        System.out.print("Enter health: ");
+        int health = scanner.nextInt();
+
+        System.out.print("Enter strength: ");
+        int strength = scanner.nextInt();
+
+        System.out.print("Enter attack: ");
+        int attack = scanner.nextInt();
+
+        return new Player(health, strength, attack);
     }
 }
