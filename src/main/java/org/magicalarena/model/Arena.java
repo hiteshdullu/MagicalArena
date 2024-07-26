9c13f6884457b6b39e410945d60d664b5ba4556a
unknown <hiteshdullu@gmail.com>
2024-07-26 15:52:48 +0530

    Adding arena entity

---
+++ src/main/java/org/magicalarena/model/Arena.java
@@ -0,0 +1,22 @@
+package org.magicalarena.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Random;
+
+@Getter
+@Setter
+
+public class Arena {
+    private Player player1;
+    private Player player2;
+    private Random random;
+
+    public Arena(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.random = new Random();
+    }
+    
+}
