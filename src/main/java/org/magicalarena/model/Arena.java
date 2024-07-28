22a0d4f39fb8ac7642de28a3cd5b687bf5f38bb9
Hitesh Dullu <hiteshdullu@gmail.com>
2024-07-28 13:43:53 +0530

    removed lombok

--- src/main/java/org/magicalarena/model/Arena.java
+++ src/main/java/org/magicalarena/model/Arena.java
@@ -1,15 +1,30 @@
 package org.magicalarena.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.Random;
-
-@Getter
-@Setter
-@AllArgsConstructor
 public class Arena {
     private Player player1;
     private Player player2;
+
+    // Constructor
+    public Arena(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    // Getter and Setter for player1
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(Player player1) {
+        this.player1 = player1;
+    }
+
+    // Getter and Setter for player2
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(Player player2) {
+        this.player2 = player2;
+    }
 }
